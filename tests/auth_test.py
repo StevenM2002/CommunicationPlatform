7ec9eb9a4cd6c48f89db8bc9137864fdb27ef45f
Joel Bryla <j.bryla@student.unsw.edu.au>
2021-09-23 11:41:23 +1000

    added extra auth tests

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -92,3 +92,38 @@ def test_multiple_register():
     auth_register_v1("email1@email.com", "password", "first", "last")
     auth_register_v1("email2@email.com", "password1", "first1", "last1")
     auth_register_v1("email3@email.com", "password2", "first2", "last2")
+
+
+def test_multiple_id():
+    clear_v1()
+    id1 = auth_register_v1("email1@email.com", "password", "first", "last")[
+        "auth_user_id"
+    ]
+    id2 = auth_register_v1("email2@email.com", "password1", "first1", "last1")[
+        "auth_user_id"
+    ]
+    id3 = auth_register_v1("email3@email.com", "password2", "first2", "last2")[
+        "auth_user_id"
+    ]
+    assert id1 != id2
+    assert id2 != id3
+    assert id1 != id3
+
+
+def test_max_length_handle():
+    clear_v1()
+
+    auth_register_v1("email@email.com", "password", "firstverylongname", "lastname")
+    user_id0 = auth_register_v1(
+        "email1@email.com", "password2", "firstverylongname", "lastname"
+    )
+    user_id1 = auth_register_v1(
+        "email2@email.com", "password3", "firstverylongname", "lastname"
+    )
+
+    users = data_store.get()["users"]
+    for user in users:
+        if user["u_id"] == user_id0:
+            assert user["handle_str"] == "firstverylongnamelas0"
+        if user["u_id"] == user_id1:
+            assert user["handle_str"] == "firstverylongnamelas1"
