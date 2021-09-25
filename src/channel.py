cf31c2ddd2e44dd202b079150abea0e9cb8cebe1
Lewis Bandas <z5359354@ad.unsw.edu.au>
2021-09-25 17:22:33 +1000

    made some small changes to tests in channel_test.py and added a working version of the channel_invite function to channel.py

--- src/channel.py
+++ src/channel.py
@@ -1,5 +1,44 @@
+from src.data_store import data_store
+from src.error import AccessError, InputError
 def channel_invite_v1(auth_user_id, channel_id, u_id):
+    
+    store = data_store.get()
+    channel_list = store["channels"]
+    user_list = store["users"]
+
+    valid_channel = False
+    for each_channel in channel_list:
+        if (each_channel["channel_id"] == channel_id):
+            valid_channel = True
+            channel = each_channel
+    if (valid_channel == False):
+        raise InputError("channel_id does not refer to a valid channel")
+
+    valid_user = False
+    for each_user in user_list:
+        if (each_user["u_id"] == u_id):
+            user_to_add = each_user
+            valid_user = True
+    if (valid_user == False):
+        raise InputError("u_id does not refer to a valid user")
+
+    valid_member = False
+    uid_in_channel = False
+    for users in channel["members"]:
+        if (users == auth_user_id):
+            valid_member = True
+        if (users == u_id):
+            uid_in_channel = True
+    if (valid_member == False):
+        raise AccessError("the authorised user is not a member of the channel")
+    if (uid_in_channel == True):
+        raise InputError("u_id refers to a user who is already a member of \
+            the channel")
+            
+    channel["members"].append(user_to_add["u_id"])
+    print(f"user id is {user_to_add}")
     return {
+
     }
 
 def channel_details_v1(auth_user_id, channel_id):
@@ -40,5 +79,18 @@ def channel_messages_v1(auth_user_id, channel_id, start):
     }
 
 def channel_join_v1(auth_user_id, channel_id):
+    #placeholder for testing
+
+    store = data_store.get()
+
+    for users in store["users"]:
+        if (users["u_id"] == auth_user_id):
+            to_add = users
+    for channels in store["channels"]:
+        if (channels["id"] == channel_id):
+            to_add_to = channels
+    
+    to_add_to.append(to_add)
     return {
+
     }
