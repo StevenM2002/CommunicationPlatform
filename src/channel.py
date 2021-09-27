b869dc052429993d5d146a2f7380af1f92841918
Riley Dean <riley@sixdeans.com>
2021-09-27 23:15:01 +1000

    Added exception handling to implementation

--- src/channel.py
+++ src/channel.py
@@ -3,7 +3,38 @@ def channel_invite_v1(auth_user_id, channel_id, u_id):
 
 
 def channel_details_v1(auth_user_id, channel_id):
+    """Finds a channel given an auth_user_id and a channel id, and returns
+    the details of the channel
 
+    Arguments:
+        auth_user_id (integer) - unique id of user
+        channel_id (integer) - unique id of channel
+
+    Exceptions:
+        InputError - Occurs when:
+            - channel_id does not refer to a valid channel
+        AccessError - Occurs when:
+            - channel_id is valid and the authorised user is not a member
+              of the channel
+
+    Return Value:
+        Returns {channel_name, owner_members, and all _members}"""
+    # Importing the data_store
+    store = data_store.get()
+    users = store["users"]
+    channels = store["channels"]
+    # Checks whether the channel_id is used 
+    valid = any(True for channel in channels if channel["channel_id"] == channel_id)
+    if not valid:
+        raise InputError("Channel_id not found")
+
+    # Checks whether auth_user_id is a member of the channel
+    is_member = any(True for user in users if user["all_members"] = user)
+    if not is_member:
+        raise AccessError("User is not a member of the channel")
+
+    # Finds the given channel, and saves the given data to a dictionary
+    
     return {
         "name": "Hayden",
         "owner_members": [
