8bacc606824150a00fa621d886d8281a12a42764
Riley Dean <riley@sixdeans.com>
2021-09-27 22:58:08 +1000

    Finalised tests to match specifications

--- src/channel.py
+++ src/channel.py
@@ -1,45 +1,46 @@
 def channel_invite_v1(auth_user_id, channel_id, u_id):
-    return {
-    }
+    return {}
+
 
 def channel_details_v1(auth_user_id, channel_id):
-    
+
     return {
-        'name': 'Hayden',
-        'owner_members': [
+        "name": "Hayden",
+        "owner_members": [
             {
-                'u_id': 1,
-                'email': 'example@gmail.com',
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-                'handle_str': 'haydenjacobs',
+                "u_id": 1,
+                "email": "example@gmail.com",
+                "name_first": "Hayden",
+                "name_last": "Jacobs",
+                "handle_str": "haydenjacobs",
             }
         ],
-        'all_members': [
+        "all_members": [
             {
-                'u_id': 1,
-                'email': 'example@gmail.com',
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-                'handle_str': 'haydenjacobs',
+                "u_id": 1,
+                "email": "example@gmail.com",
+                "name_first": "Hayden",
+                "name_last": "Jacobs",
+                "handle_str": "haydenjacobs",
             }
         ],
     }
+
 
 def channel_messages_v1(auth_user_id, channel_id, start):
     return {
-        'messages': [
+        "messages": [
             {
-                'message_id': 1,
-                'u_id': 1,
-                'message': 'Hello world',
-                'time_created': 1582426789,
+                "message_id": 1,
+                "u_id": 1,
+                "message": "Hello world",
+                "time_created": 1582426789,
             }
         ],
-        'start': 0,
-        'end': 50,
+        "start": 0,
+        "end": 50,
     }
 
+
 def channel_join_v1(auth_user_id, channel_id):
-    return {
-    }
+    return {}
