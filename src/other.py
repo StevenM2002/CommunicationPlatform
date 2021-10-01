14a80ca29a8ebeb047b95cbe07329b841968a060
Daniel O'Connell <daniel.oconnell@student.unsw.edu.au>
2021-10-01 13:15:54 +1000

    Update style and add more tests for data_store

--- src/other.py
+++ src/other.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
-from src.data_store import data_store, initial_object
+from src.data_store import data_store, INITIAL_OBJECT
 
 
 def clear_v1():
-    data_store.set(deepcopy(initial_object))
+    data_store.set(deepcopy(INITIAL_OBJECT))
