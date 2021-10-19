f6ba65e4bb98a78bf621164f5364fc8d1e76a6f3
Daniel O'Connell <daniel.oconnell@student.unsw.edu.au>
2021-10-19 11:27:47 +1100

    Update datastore to be persistent and remove tests

--- src/data_store.py
+++ src/data_store.py
@@ -49,15 +49,26 @@ data structure:
     data_store.set(users)
 """
 from copy import deepcopy
+from json import dump, load
+from pathlib import Path
+from threading import Event, Thread
 
 INITIAL_OBJECT = {"users": [], "channels": [], "global_owners": []}
+DATA_STORE_FILE = "datastore.json"
+WRITE_INTERVAL = 30
 
 
 class Datastore:
     """Datastore class used to store data for Streams."""
 
     def __init__(self):
-        self.__store = deepcopy(INITIAL_OBJECT)
+        if Path(DATA_STORE_FILE).is_file():
+            try:
+                self.__store = load(open(DATA_STORE_FILE))
+            except:
+                self.__store = deepcopy(INITIAL_OBJECT)
+        else:
+            self.__store = deepcopy(INITIAL_OBJECT)
 
     def get(self):
         """Get the dictionary of the data base.
@@ -80,4 +91,29 @@ class Datastore:
         self.__store = store
 
 
+def every(interval):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            stopped = Event()
+
+            def loop():
+                while not stopped.wait(interval):
+                    func(*args, **kwargs)
+
+            t = Thread(target=loop)
+            t.daemon = True
+            t.start()
+            return stopped
+
+        return wrapper
+
+    return decorator
+
+
+@every(WRITE_INTERVAL)
+def save_data_store():
+    dump(data_store.get(), open(DATA_STORE_FILE, "w"))
+
+
 data_store = Datastore()
+save_data_store()
--- src/server.py
+++ src/server.py
@@ -1,46 +1,43 @@
-import sys
 import signal
 from json import dumps
-from flask import Flask, request
+from flask import Flask
 from flask_cors import CORS
-from src.error import InputError
 from src import config
+from src.other import clear_v1
 
-def quit_gracefully(*args):
-    '''For coverage'''
+
+def quit_gracefully(*_):
+    """For coverage"""
     exit(0)
+
 
 def defaultHandler(err):
     response = err.get_response()
-    print('response', err, err.get_response())
-    response.data = dumps({
-        "code": err.code,
-        "name": "System Error",
-        "message": err.get_description(),
-    })
-    response.content_type = 'application/json'
+    print("response", err, err.get_response())
+    response.data = dumps(
+        {
+            "code": err.code,
+            "name": "System Error",
+            "message": err.get_description(),
+        }
+    )
+    response.content_type = "application/json"
     return response
+
 
 APP = Flask(__name__)
 CORS(APP)
 
-APP.config['TRAP_HTTP_EXCEPTIONS'] = True
+APP.config["TRAP_HTTP_EXCEPTIONS"] = True
 APP.register_error_handler(Exception, defaultHandler)
 
-#### NO NEED TO MODIFY ABOVE THIS POINT, EXCEPT IMPORTS
 
-# Example
-@APP.route("/echo", methods=['GET'])
-def echo():
-    data = request.args.get('data')
-    if data == 'echo':
-   	    raise InputError(description='Cannot echo "echo"')
-    return dumps({
-        'data': data
-    })
+@APP.route("/clear", methods=["DELETE"])
+def clear():
+    clear_v1()
+    return {}
 
-#### NO NEED TO MODIFY BELOW THIS POINT
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, quit_gracefully) # For coverage
-    APP.run(port=config.port) # Do not edit this port
+    signal.signal(signal.SIGINT, quit_gracefully)  # For coverage
+    APP.run(port=config.port)  # Do not edit this port
