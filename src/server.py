1fc716cc19e336139e030bf960e4d717c8e27fd0
Riley Dean <riley@sixdeans.com>
2021-10-19 11:00:52 +1100

    Converted v1 tests to http tests

--- src/server.py
+++ src/server.py
@@ -5,42 +5,52 @@ from flask import Flask, request
 from flask_cors import CORS
 from src.error import InputError
 from src import config
+from src.channels import channels_create_v2
+
 
 def quit_gracefully(*args):
-    '''For coverage'''
+    """For coverage"""
     exit(0)
+
 
 def defaultHandler(err):
     response = err.get_response()
-    print('response', err, err.get_response())
-    response.data = dumps({
-        "code": err.code,
-        "name": "System Error",
-        "message": err.get_description(),
-    })
-    response.content_type = 'application/json'
+    print("response", err, err.get_response())
+    response.data = dumps(
+        {
+            "code": err.code,
+            "name": "System Error",
+            "message": err.get_description(),
+        }
+    )
+    response.content_type = "application/json"
     return response
+
 
 APP = Flask(__name__)
 CORS(APP)
 
-APP.config['TRAP_HTTP_EXCEPTIONS'] = True
+APP.config["TRAP_HTTP_EXCEPTIONS"] = True
 APP.register_error_handler(Exception, defaultHandler)
 
 #### NO NEED TO MODIFY ABOVE THIS POINT, EXCEPT IMPORTS
 
 # Example
-@APP.route("/echo", methods=['GET'])
+@APP.route("/echo", methods=["GET"])
 def echo():
-    data = request.args.get('data')
-    if data == 'echo':
-   	    raise InputError(description='Cannot echo "echo"')
-    return dumps({
-        'data': data
-    })
+    data = request.args.get("data")
+    if data == "echo":
+        raise InputError(description='Cannot echo "echo"')
+    return dumps({"data": data})
+
+
+@APP.route("/channels/create/v2", methods=["POST"])
+def create_channel_v2():
+    data = request.get_json()
+
 
 #### NO NEED TO MODIFY BELOW THIS POINT
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, quit_gracefully) # For coverage
-    APP.run(port=config.port) # Do not edit this port
+    signal.signal(signal.SIGINT, quit_gracefully)  # For coverage
+    APP.run(port=config.port)  # Do not edit this port
