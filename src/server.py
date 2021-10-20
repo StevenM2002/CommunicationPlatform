5f9a9e4877f4058bff3a7fdc16cbfa84b8300065
Joel Bryla <j.bryla@student.unsw.edu.au>
2021-10-21 00:54:29 +1100

    added functions to server

--- src/server.py
+++ src/server.py
@@ -2,7 +2,7 @@ import signal
 from json import dumps
 from flask import Flask, request
 from flask_cors import CORS
-from src import config, auth
+from src import config, auth, dm
 from src.other import clear_v1
 
 
@@ -41,7 +41,6 @@ def auth_login():
 @APP.route("/auth/register/v2", methods=["POST"])
 def auth_register():
     data = request.json
-    print(data["email"])
     return dumps(
         auth.auth_register_v2(
             data["email"], data["password"], data["name_first"], data["name_last"]
@@ -54,6 +53,42 @@ def auth_logout():
     data = request.json
     auth.auth_logout_v1(data["token"])
     return {}
+
+
+@APP.route("/dm/create/v1", methods=["POST"])
+def dm_create():
+    data = request.json
+    return dumps(dm.dm_create_v1(data["token"], data["u_ids"]))
+
+
+@APP.route("/dm/list/v1", methods=["GET"])
+def dm_list():
+    data = request.json
+    return dumps(dm.dm_list_v1(data["token"]))
+
+
+@APP.route("/dm/remove/v1", methods=["DELETE"])
+def dm_remove():
+    data = request.json
+    return dumps(dm.dm_remove_v1(data["token"], data["dm_id"]))
+
+
+@APP.route("/dm/details/v1", methods=["GET"])
+def dm_details():
+    data = request.json
+    return dumps(dm.dm_details_v1(data["token"], data["dm_id"]))
+
+
+@APP.route("/dm/leave/v1", methods=["POST"])
+def dm_leave():
+    data = request.json
+    return dumps(dm.dm_leave_v1(data["token"], data["dm_id"]))
+
+
+@APP.route("/dm/messages/v1", methods=["GET"])
+def dm_messages():
+    data = request.json
+    return dumps(dm.dm_messages_v1(data["token"], data["dm_id"], data["start"]))
 
 
 @APP.route("/clear/v1", methods=["DELETE"])
