866ae92d5d09c862b2f776f5550e46af5bbb19b9
Lewis Bandas <l.bandas@student.unsw.edu.au>
2021-10-21 22:48:08 +1100

    removed jwt decoding from server file

--- src/server.py
+++ src/server.py
@@ -3,13 +3,11 @@ from json import dumps
 from flask import Flask, request
 from flask_cors import CORS
 from src.channels import channels_create_v2
-from src.channel import channel_details_v2
+from src.channel import channel_details_v2, channel_invite_v2, channel_join_v2
 from src import config, auth, dm
 from src.other import clear_v1
 from src.error import InputError, AccessError
-import jwt
-from src.auth import JWT_SECRET
-from src.channel import channel_invite_v1, channel_join_v1
+
 
 def quit_gracefully(*args):
     """For coverage"""
@@ -115,24 +113,19 @@ def get_channel_details():
     return dumps(channel_details_v2(token, channel_id))
 
 @APP.route('/channel/invite/v2', methods=['POST'])
-def channel_invite_v2():
+def do_channel_invite():
     params = request.get_json()
     token = params['token']
     channel_id = params['channel_id']
     u_id = params['u_id']
-    decoded_token = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
-    auth_id = decoded_token['u_id']
-    return dumps(channel_invite_v1(auth_id, channel_id, u_id))
+    return dumps(channel_invite_v2(token, channel_id, u_id))
 
 @APP.route('/channel/join/v2', methods=['POST'])
-def channel_join_v2():
+def do_channel_join():
     params = request.get_json()
     token = params['token']
     channel_id = params['channel_id']
-    decoded_token = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
-    auth_id = decoded_token['u_id']
-    return dumps(channel_join_v1(auth_id, channel_id))
-
+    return dumps(channel_join_v2(token, channel_id))
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, quit_gracefully)  # For coverage
