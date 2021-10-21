140329bb22ac14cb77f0792dc15f0c0e36289455
Lewis Bandas <l.bandas@student.unsw.edu.au>
2021-10-21 15:37:32 +1100

    fully working versions of channel join and invite v2

--- src/server.py
+++ src/server.py
@@ -1,46 +1,102 @@
-import sys
 import signal
 from json import dumps
 from flask import Flask, request
 from flask_cors import CORS
-from src.error import InputError
-from src import config
-from src.channel import channel_invite_v1, channel_join_v1
+from src import config, auth, dm
+from src.other import clear_v1
 import jwt
-from src.admin import admin_user_remove_v1, admin_user_permission_change_v1
+from src.channel import channel_invite_v1, channel_join_v1
 from src.auth import JWT_SECRET
-def quit_gracefully(*args):
-    '''For coverage'''
+from src.admin import admin_user_permission_change_v1, admin_user_remove_v1
+def quit_gracefully(*_):
+    """For coverage"""
     exit(0)
+
 
 def defaultHandler(err):
     response = err.get_response()
-    print('response', err, err.get_response())
-    response.data = dumps({
-        "code": err.code,
-        "name": "System Error",
-        "message": err.get_description(),
-    })
-    response.content_type = 'application/json'
+    print("response", err, err.get_response())
+    response.data = dumps(
+        {
+            "code": err.code,
+            "name": "System Error",
+            "message": err.get_description(),
+        }
+    )
+    response.content_type = "application/json"
     return response
+
 
 APP = Flask(__name__)
 CORS(APP)
 
-APP.config['TRAP_HTTP_EXCEPTIONS'] = True
+APP.config["TRAP_HTTP_EXCEPTIONS"] = True
 APP.register_error_handler(Exception, defaultHandler)
 
-#### NO NEED TO MODIFY ABOVE THIS POINT, EXCEPT IMPORTS
 
-# Example
-@APP.route("/echo", methods=['GET'])
-def echo():
-    data = request.args.get('data')
-    if data == 'echo':
-   	    raise InputError(description='Cannot echo "echo"')
-    return dumps({
-        'data': data
-    })
+@APP.route("/auth/login/v2", methods=["POST"])
+def auth_login():
+    data = request.json
+    return dumps(auth.auth_login_v2(data["email"], data["password"]))
+
+
+@APP.route("/auth/register/v2", methods=["POST"])
+def auth_register():
+    data = request.json
+    return dumps(
+        auth.auth_register_v2(
+            data["email"], data["password"], data["name_first"], data["name_last"]
+        )
+    )
+
+
+@APP.route("/auth/logout/v1", methods=["POST"])
+def auth_logout():
+    data = request.json
+    auth.auth_logout_v1(data["token"])
+    return {}
+
+
+@APP.route("/dm/create/v1", methods=["POST"])
+def dm_create():
+    data = request.json
+    return dumps(dm.dm_create_v1(data["token"], data["u_ids"]))
+
+
+@APP.route("/dm/list/v1", methods=["GET"])
+def dm_list():
+    data = request.json
+    return dumps(dm.dm_list_v1(data["token"]))
+
+
+@APP.route("/dm/remove/v1", methods=["DELETE"])
+def dm_remove():
+    data = request.json
+    return dumps(dm.dm_remove_v1(data["token"], data["dm_id"]))
+
+
+@APP.route("/dm/details/v1", methods=["GET"])
+def dm_details():
+    data = request.json
+    return dumps(dm.dm_details_v1(data["token"], data["dm_id"]))
+
+
+@APP.route("/dm/leave/v1", methods=["POST"])
+def dm_leave():
+    data = request.json
+    return dumps(dm.dm_leave_v1(data["token"], data["dm_id"]))
+
+
+@APP.route("/dm/messages/v1", methods=["GET"])
+def dm_messages():
+    data = request.json
+    return dumps(dm.dm_messages_v1(data["token"], data["dm_id"], data["start"]))
+
+
+@APP.route("/clear/v1", methods=["DELETE"])
+def clear():
+    clear_v1()
+    return {}
 
 @APP.route('/channel/invite/v2', methods=['POST'])
 def channel_invite_v2():
@@ -48,7 +104,7 @@ def channel_invite_v2():
     token = params['token']
     channel_id = params['channel_id']
     u_id = params['u_id']
-    decoded_token = jwt.decode(token, JWT_SECRET, algorithm='HS256')
+    decoded_token = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
     auth_id = decoded_token['u_id']
     return dumps(channel_invite_v1(auth_id, channel_id, u_id))
 
@@ -57,12 +113,11 @@ def channel_join_v2():
     params = request.get_json()
     token = params['token']
     channel_id = params['channel_id']
-    decoded_token = jwt.decode(token, JWT_SECRET, algorithm='HS256')
+    decoded_token = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
     auth_id = decoded_token['u_id']
     return dumps(channel_join_v1(auth_id, channel_id))
 
-#### NO NEED TO MODIFY BELOW THIS POINT
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, quit_gracefully) # For coverage
-    APP.run(port=config.port) # Do not edit this port
+    signal.signal(signal.SIGINT, quit_gracefully)  # For coverage
+    APP.run(port=config.port)  # Do not edit this port
