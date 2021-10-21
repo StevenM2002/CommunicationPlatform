8415c5f187b4e521eb37e5719ab30db111e52db8
Riley Dean <riley@sixdeans.com>
2021-10-21 15:59:19 +1100

    Fixed import error

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -3,12 +3,12 @@ import pytest
 
 from src.error import InputError, AccessError
 from src.channel import (
-    channel_details_v1,
+    channel_details_v2,
     channel_join_v1,
     channel_invite_v1,
     channel_messages_v1,
 )
-from src.channels import channels_create_v1
+from src.channels import channels_create_v2
 from src.other import clear_v1
 from src.auth import auth_register_v2 as auth_register_v1
 from src.data_store import data_store
