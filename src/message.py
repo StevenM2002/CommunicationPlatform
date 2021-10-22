f819133c7e897dd576ef8ac6340136171172a6d1
Daniel O'Connell <daniel.oconnell@student.unsw.edu.au>
2021-10-22 15:27:51 +1100

    Move message code to separate file and add iteration 2 code

---
+++ src/message.py
@@ -0,0 +1,104 @@
+import math
+import time
+
+from src.data_store import Channel, Message, data_store
+from src.error import AccessError, InputError
+from src.other import first, validate_auth_id
+
+
+@validate_auth_id
+def channel_messages_v1(auth_user_id, channel_id, start):
+    """Get the messages from a channels.
+
+    Arguments:
+        auth_user_id (integer) - id of user requesting messages
+        channel_id (integer) - id of channel to get messages from
+        start (integer) - index of first message to start from
+
+    Exceptions:
+        AccessError - Occurs when:
+            - auth_user_id does not belong to a user
+            - channel_id is valid and the authorised user is not a member of
+            the channel
+        InputError - Occurs when:
+            - channel_id does not refer to a valid channel
+            - start is greater than the total number of messages in the channel
+
+    Returns:
+        Returns {messages, start, end}
+    """
+    # channel is set to the channel that matches the given channel_id if none
+    # match then it is set to an empty dictionary
+    channels = data_store.get()["channels"]
+    channel = first(lambda c: c["channel_id"] == channel_id, channels, {})
+    if not channel:
+        raise InputError(description="no channel matching channel id")
+    if not auth_user_id in channel["all_members"]:
+        raise AccessError(description="user is not a member of this channel")
+    messages = len(channel["messages"])
+    if start > messages:
+        raise InputError(description="start message id is greater than latest message id")
+
+    # end is set to -1 if the most recent message has been returned
+    return {
+        "messages": channel["messages"][start : start + 50],
+        "start": start,
+        "end": start + 50 if start + 50 < messages else -1,
+    }
+
+
+def message_send_v1(user_id, channel_id, message_text):
+    data = data_store.get()
+    channel = first(lambda c: c["channel_id"] == channel_id, data["channels"], {})
+    if not channel:
+        raise InputError(description="no channel matching channel id")
+    if not user_id in channel["all_members"]:
+        raise AccessError(description="user is not a member of this channel")
+    if len(message_text) > 1000:
+        raise InputError(description="message longer than 1000 characters")
+    if len(message_text) == 0:
+        raise InputError(description="message empty must contain 1 characters")
+    message_id = data["max_message_id"] + 1
+    data["max_message_id"] += 1
+    message = {
+        "message": message_text,
+        "message_id": message_id,
+        "time_created": math.floor(time.time()),
+        "u_id": user_id,
+    }
+    channel["messages"].insert(0, message)
+    return {"message_id": message_id}
+
+
+def get_message(message_id) -> tuple[Message, Channel]:
+    data = data_store.get()
+    for channel in data["channels"]:
+        for message in channel["messages"]:
+            if message["message_id"] == message_id:
+                return message, channel
+    raise InputError(description="no message with message id was found")
+
+
+def message_edit_v1(user_id, message_id, edited_message):
+    message, channel = get_message(message_id)
+    if message["u_id"] != user_id and user_id not in channel["owner_members"]:
+        raise AccessError(
+            description="message not sent by user or user not owner of channel"
+        )
+    if len(edited_message) > 1000:
+        raise InputError(description="message longer than 1000 characters")
+    if edited_message == "":
+        message_remove_v1(user_id, message_id)
+    else:
+        message["message"] = edited_message
+    return {}
+
+
+def message_remove_v1(user_id, message_id):
+    message, channel = get_message(message_id)
+    if message["u_id"] != user_id and user_id not in channel["owner_members"]:
+        raise AccessError(
+            description="message not sent by user or user not owner of channel"
+        )
+    channel["messages"].remove(message)
+    return {}
