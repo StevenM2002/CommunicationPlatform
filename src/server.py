71c7b1f44c681c0c8dcec8ee83cb7bd5ec9cd035
Steven Mai <stevenm@stevenm-mac.local>
2021-11-12 13:05:59 +1100

    server added

--- src/server.py
+++ src/server.py
@@ -25,6 +25,7 @@ from src.user import (
     user_set_email,
     user_set_handle,
 )
+from src.search import search_v1
 
 from flask import Flask, request
 from flask_cors import CORS
@@ -270,6 +271,12 @@ def do_admin_userpermission_change():
     u_id = params["u_id"]
     permission_id = params["permission_id"]
     return dumps(admin_user_permission_change_v1(token, u_id, permission_id))
+
+@APP.route("/search/v1", methods=["GET"])
+def search_the_messages():
+    token = request.args.get("token")
+    query_str = request.args.get("query_str")
+    return dumps(search_v1(token, query_str))
 
 
 if __name__ == "__main__":
