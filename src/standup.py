d7116409db6f39df5547668c0c7e48b39a838b1e
Riley Dean <riley@sixdeans.com>
2021-11-14 12:00:58 +1100

    Changed standup.py to include stats functionality

--- src/standup.py
+++ src/standup.py
@@ -6,24 +6,30 @@ import math
 import threading
 from src.data_store import data_store
 from src.error import InputError, AccessError
+from src.message import increment_workspace_messages, increment_user_messages
+
+
 def is_valid_channel(channels, channel_id):
     for channel in channels:
-        if channel['channel_id'] == channel_id:
+        if channel["channel_id"] == channel_id:
             return True
     return False
-    
+
+
 def auth_not_member(channels, channel_id, auth_user_id):
     for channel in channels:
-        if channel['channel_id'] == channel_id:
+        if channel["channel_id"] == channel_id:
             standup_channel = channel
-    return auth_user_id not in standup_channel['all_members']
+    return auth_user_id not in standup_channel["all_members"]
+
 
 def end_standup(auth_user_id, channel, standup):
     data = data_store.get()
+
     message_id = data["max_ids"]["message"] + 1
     data["max_ids"]["message"] += 1
     standup["message_queue"] = standup["message_queue"][:-1]
-    message =  {
+    message = {
         "message": standup["message_queue"],
         "message_id": message_id,
         "time_created": math.floor(time.time()),
@@ -33,26 +39,36 @@ def end_standup(auth_user_id, channel, standup):
     for standups in data["standups"]:
         if standups == standup:
             data["standups"].remove(standup)
-        
+    # Increments the workspace stats, as well as the user stats for the user
+    # who initiated the standup
+    increment_workspace_messages()
+    increment_user_messages(auth_user_id)
+
     data_store.set(data)
+
+
 def standup_start_v1(token, channel_id, length):
     store = data_store.get()
-    auth_user_id = extract_token(token)['u_id']
-    channels = store['channels']
-    
+    auth_user_id = extract_token(token)["u_id"]
+    channels = store["channels"]
+
     if not is_valid_channel(channels, channel_id):
         raise InputError("channel_id does not refer to a valid channel")
-    
-    if auth_not_member(channels, channel_id, auth_user_id):
-        raise AccessError("channel_id is valid and the authorised user \
-            is not a member of the channel")
 
-    if (not length >= 0):
+    if auth_not_member(channels, channel_id, auth_user_id):
+        raise AccessError(
+            "channel_id is valid and the authorised user \
+            is not a member of the channel"
+        )
+
+    if not length >= 0:
         raise InputError("length is a negative integer")
 
-    if standup_active_v1(token, channel_id)['is_active']:
-        raise InputError("an active standup is currently running \
-            in the channel")
+    if standup_active_v1(token, channel_id)["is_active"]:
+        raise InputError(
+            "an active standup is currently running \
+            in the channel"
+        )
 
     for channel in channels:
         if channel["channel_id"] == channel_id:
@@ -65,52 +81,60 @@ def standup_start_v1(token, channel_id, length):
     new_standup = {
         "channel_id": channel_id,
         "time_finish": timestamp,
-        "message_queue": ""
+        "message_queue": "",
     }
     standups.append(new_standup)
 
     data_store.set(store)
-    threading.Timer(length, end_standup, [auth_user_id, standup_channel, \
-        new_standup]).start()
+    threading.Timer(
+        length, end_standup, [auth_user_id, standup_channel, new_standup]
+    ).start()
     return {"time_finish": timestamp}
+
+
 def standup_active_v1(token, channel_id):
     store = data_store.get()
     channels = store["channels"]
-    auth_user_id = extract_token(token)['u_id']
-    
+    auth_user_id = extract_token(token)["u_id"]
+
     if not is_valid_channel(channels, channel_id):
         raise InputError("channel_id does not refer to a valid channel")
 
     if auth_not_member(channels, channel_id, auth_user_id):
-        raise AccessError("channel_id is valid and the authorised user \
-            is not a member of the channel")
-    
+        raise AccessError(
+            "channel_id is valid and the authorised user \
+            is not a member of the channel"
+        )
+
     for standup in store["standups"]:
-        if (standup["channel_id"] == channel_id and standup["time_finish"] \
-            > datetime.datetime.now().replace(tzinfo=timezone.utc).timestamp()):
-            return {
-                "is_active": True,
-                "time_finish": standup["time_finish"]
-            }
-    return {
-        "is_active": False, 
-        "time_finish": None
-    }
+        if (
+            standup["channel_id"] == channel_id
+            and standup["time_finish"]
+            > datetime.datetime.now().replace(tzinfo=timezone.utc).timestamp()
+        ):
+            return {"is_active": True, "time_finish": standup["time_finish"]}
+    return {"is_active": False, "time_finish": None}
+
+
 def standup_send_v1(token, channel_id, message):
     store = data_store.get()
     channels = store["channels"]
-    auth_user_id = extract_token(token)['u_id']
+    auth_user_id = extract_token(token)["u_id"]
 
     if not is_valid_channel(channels, channel_id):
         raise InputError("channel_id does not refer to a valid channel")
 
     if auth_not_member(channels, channel_id, auth_user_id):
-        raise AccessError("channel_id is valid and the authorised user is \
-            not a member of the channel")
+        raise AccessError(
+            "channel_id is valid and the authorised user is \
+            not a member of the channel"
+        )
 
     if not standup_active_v1(token, channel_id)["is_active"]:
-        raise InputError("an active standup is not currently running in \
-            the channel")
+        raise InputError(
+            "an active standup is not currently running in \
+            the channel"
+        )
 
     if len(message) > 1000:
         raise InputError("length of message is over 1000 characters")
@@ -123,4 +147,3 @@ def standup_send_v1(token, channel_id, message):
     standup["message_queue"] += f"{name}: {message}\n"
     data_store.set(store)
     return {}
-    
